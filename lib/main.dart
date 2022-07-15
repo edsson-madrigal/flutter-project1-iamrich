77d8ffb6fd24a061a720e86c98809d836644bfff
Edsson <akairu@hotmail.com>
2022-07-14 23:34:59 -0500

    scaffold, material colors, widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello world"),
-      ),
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            title: const Text("i am rich"),
+            backgroundColor: Colors.red[800],
+          ),
+          body: Center(
+            child: Image.network(
+                "https://lh3.googleusercontent.com/6rI6tTsI2UXRbr_Br7JoYYTRFOnfqyMhzgXM3JOSchbUpAspYniZty2aocD5vdxMSnlx"),
+          )),
     ),
   );
 }
